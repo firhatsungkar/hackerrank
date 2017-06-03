f19e0d99646c9cafdfcf42268848f1db6f8728fc
Muhamad Firhat <muhamad.firhat@gmail.com>
2017-06-03 16:21:30 +0700

    maximum-element updated

--- maximum-element/index.js
+++ maximum-element/index.js
@@ -1,24 +1,36 @@
 function processData(input) {
     //Enter your code here
-    const inputs = input.split('\n');
-    const stack = []
-    inputs.forEach((input, index) => {
-        if(index > 0) {
-            switch(parseInt(input[0])) {
-                case 1:
-                    var value = input.split(' ')
-                    stack.push(value[1])
-                    break
-                case 2:
-                    stack.pop()
-                    break
-                case 3:
-                    var sortedStack = (Array.from(stack)).sort((a,b) => b-a)
-                    console.log(sortedStack[0])
-                    break
-            }
+    // Stack Class
+    function Stack() {
+        var collections = [];
+        this.push = (element) => collections.push(element);
+        this.pop = () => collections.pop();
+        this.max = () => {
+            console.log( Array.from(collections).sort((a,b)=> a-b)[collections.length - 1] )
         }
-    })
+    };
+    // Initiate new stack and Desctructuring input
+    var stack = new Stack();
+    var input = input.split('\n');
+    var max = input.shift();
+    
+    // Execute
+    for(i = 0; i <= max -1; i++) {
+        var type = input[i].split(' ')[0]
+        var value = input[i].split(' ')[1]
+        switch(parseInt(type)) {
+            case 1:
+                stack.push(value);
+                break;
+            case 2:
+                stack.pop();
+                break;
+            case 3:
+                stack.max();
+                break;
+        }
+    };
+    
 } 
 
 process.stdin.resume();
